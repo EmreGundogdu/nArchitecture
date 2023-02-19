c3c11718a4a399aafd7eb56f037c60b17fba3abb
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-02-19 20:02:44 +0300

    Reference added to entity layer from persistence layer

--- src/demoProjects/templateProject/Domain/Entities/SomeFeatureEntity.cs
+++ src/demoProjects/templateProject/Domain/Entities/SomeFeatureEntity.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Core.Persistence.Repositories;
 
 namespace Domain.Entities
 {
-    public class SomeFeatureEntity
+    public class SomeFeatureEntity : Entity
     {
         public string Name { get; set; }
 
