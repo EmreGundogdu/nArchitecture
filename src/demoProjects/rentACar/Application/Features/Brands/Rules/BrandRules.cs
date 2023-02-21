5985038a7ddf47909974a40d8dd740ffbf3d3bbb
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-02-21 23:46:43 +0300

    BrandRule added

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Rules/BrandRules.cs
@@ -0,0 +1,23 @@
+﻿using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+using Core.Persistence.Paging;
+using Domain.Entities;
+
+namespace Application.Features.someFeature.Rules
+{
+    public class BrandRules
+    {
+        private readonly IBrandRepository brandRepository;
+
+        public BrandRules(IBrandRepository brandRepository)
+        {
+            this.brandRepository = brandRepository;
+        }
+
+        public async Task BrandNameCanNotBeDuplicatedWhenInserted(string name)
+        {
+            IPaginate<Brand> result = await brandRepository.GetListAsync(b => b.Name == name);
+            if (result.Items.Any()) throw new BusinessException("Brand name exists.");
+        }
+    }
+}
