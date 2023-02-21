9e8493ea66865c2854cdc81ea613cc2d296b102e
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-02-21 22:25:41 +0300

    CreateBrandCommandHandler added

--- src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
@@ -1,8 +1,17 @@
-﻿using MediatR;
+﻿using Application.Features.Brands.Dtos;
+using MediatR;
 
 namespace Application.Features.Brands.Commands.CreateBrand
 {
-    public class CreateBrandCommand : IRequest
+    public class CreateBrandCommand : IRequest<CreatedBrandDto>
     {
+        public string Name { get; set; }
+        public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandDto>
+        {
+            public Task<CreatedBrandDto> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
