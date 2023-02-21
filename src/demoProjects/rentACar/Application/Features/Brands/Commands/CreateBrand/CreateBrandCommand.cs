273473d9f5af6ee6702996be7e0a6d1f7ecb06f3
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-02-21 22:23:29 +0300

    CreateBranddto added

--- src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
 
 namespace Application.Features.Brands.Commands.CreateBrand
 {
-    internal class CreateBrandCommand
+    public class CreateBrandCommand : IRequest
     {
     }
 }
