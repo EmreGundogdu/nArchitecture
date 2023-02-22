0a92a590e2c2204f7c857f276a4a3ab686da08b5
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-02-23 01:07:22 +0300

    Auth feature added to application layer

---
+++ src/demoProjects/rentACar/Application/Features/Auth/Commands/Register/RegisterCommand.cs
@@ -0,0 +1,35 @@
+﻿using Application.Features.Auth.Dtos;
+using Application.Features.Auth.Rules;
+using Application.Services.Repositories;
+using Core.Security.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Auth.Commands.Register
+{
+    public class RegisterCommand : IRequest<RegisteredDto>
+    {
+        public UserForRegisterDto UserForRegisterDto { get; set; }
+        public string IpAddress { get; set; }
+    }
+    public class RegisterCommandHandler : IRequestHandler<RegisterCommand, RegisteredDto>
+    {
+        private readonly AuthBusinessRules authBusinessRules;
+        readonly IUserRepository userRepository;
+
+        public RegisterCommandHandler(AuthBusinessRules authBusinessRules, IUserRepository userRepository)
+        {
+            this.authBusinessRules = authBusinessRules;
+            this.userRepository = userRepository;
+        }
+
+        public async Task<RegisteredDto> Handle(RegisterCommand request, CancellationToken cancellationToken)
+        {
+            await authBusinessRules.EmailCanNotBeDuplicatedWhenRegistered(request.UserForRegisterDto.Email);
+        }
+    }
+}
---
+++ src/demoProjects/rentACar/Application/Features/Auth/Rules/AuthBusinessRules.cs
@@ -0,0 +1,28 @@
+﻿using Application.Services.Repositories;
+using Core.Security.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Auth.Rules
+{
+    public class AuthBusinessRules
+    {
+        private readonly IUserRepository userRepository;
+
+        public AuthBusinessRules(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+        public async Task EmailCanNotBeDuplicatedWhenRegistered(string email)
+        {
+            User? user = await userRepository.GetAsync(x=>x.Email==email);
+            if (user != null)
+            {
+                throw new Exception("Mail already exists!");
+            }
+        }
+    }
+}
