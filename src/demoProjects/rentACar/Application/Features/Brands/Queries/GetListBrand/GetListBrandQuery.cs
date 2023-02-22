2d0109ac62c2b4f98bfaed680bac171a86329ade
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-02-22 21:03:27 +0300

    GetListBrandQuery and  GetListBrandQueryHandler added

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Queries/GetListBrand/GetListBrandQuery.cs
@@ -0,0 +1,21 @@
+﻿using Application.Features.Brands.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Queries.GetListBrand
+{
+    public class GetListBrandQuery : IRequest<BrandListDto>
+    {
+    }
+    public class GetListBrandQueryHandler : IRequestHandler<GetListBrandQuery, BrandListDto>
+    {
+        public Task<BrandListDto> Handle(GetListBrandQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
