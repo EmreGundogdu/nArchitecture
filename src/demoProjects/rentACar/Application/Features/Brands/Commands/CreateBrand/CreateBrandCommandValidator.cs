7e52c4688ac42d1f7a14ce19b141545459374be6
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-02-22 21:48:36 +0300

    CreateBrandCommandValidator added

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.CreateBrand
+{
+    public class CreateBrandCommandValidator:AbstractValidator<CreateBrandCommand>
+    {
+        public CreateBrandCommandValidator()
+        {
+            RuleFor(x => x.Name).NotNull().NotEmpty();
+        }
+    }
+}
