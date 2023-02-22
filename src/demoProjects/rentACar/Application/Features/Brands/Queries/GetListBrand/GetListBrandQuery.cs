5f84f43f7b998749564701696c83bbb0b95a538a
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-02-22 21:16:39 +0300

    GetListBrndQuery updated

--- src/demoProjects/rentACar/Application/Features/Brands/Queries/GetListBrand/GetListBrandQuery.cs
+++ src/demoProjects/rentACar/Application/Features/Brands/Queries/GetListBrand/GetListBrandQuery.cs
@@ -1,4 +1,8 @@
 ﻿using Application.Features.Brands.Dtos;
+using Application.Features.Brands.Models;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Application.Requests;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -10,12 +14,24 @@ namespace Application.Features.Brands.Queries.GetListBrand
 {
     public class GetListBrandQuery : IRequest<BrandListDto>
     {
+        public PageRequest PageRequest { get; set; }
     }
     public class GetListBrandQueryHandler : IRequestHandler<GetListBrandQuery, BrandListDto>
     {
-        public Task<BrandListDto> Handle(GetListBrandQuery request, CancellationToken cancellationToken)
+        IBrandRepository brandRepository;
+        IMapper mapper;
+
+        public GetListBrandQueryHandler(IBrandRepository brandRepository, IMapper mapper)
         {
-            throw new NotImplementedException();
+            this.brandRepository = brandRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<BrandListDto> Handle(GetListBrandQuery request, CancellationToken cancellationToken)
+        {
+            var brands = await brandRepository.GetListAsync(index:request.PageRequest.Page,size: request.PageRequest.PageSize);
+            BrandListModel mappedBrandList = mapper.Map<BrandListModel>(brands);
+            return mappedBrandList;
         }
     }
 }
