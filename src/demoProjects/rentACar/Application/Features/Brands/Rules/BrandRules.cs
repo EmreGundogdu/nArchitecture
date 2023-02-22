6a7f2a74719cf0f6493bcef78e59794dd94d7bbd
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-02-22 21:59:41 +0300

    BrandShouldExistWhenRequested rule added

--- src/demoProjects/rentACar/Application/Features/Brands/Rules/BrandRules.cs
+++ src/demoProjects/rentACar/Application/Features/Brands/Rules/BrandRules.cs
@@ -19,5 +19,11 @@ namespace Application.Features.someFeature.Rules
             IPaginate<Brand> result = await brandRepository.GetListAsync(b => b.Name == name);
             if (result.Items.Any()) throw new BusinessException("Brand name exists.");
         }
+
+        public async Task BrandShouldExistWhenRequested(int id)
+        {
+            Brand result = await brandRepository.GetAsync(b => b.Id == id);
+            if (result == null) throw new BusinessException("Requested brand not exists.");
+        }
     }
 }
